4b3aa3d6c727b97d653f82d26f4f6eb7cadbc85e
Lucas Eduardo <lucasatletico@hotmail.com>
2020-10-30 23:18:19 -0300

    Criando repository e service para categoria

---
+++ src/main/java/silva/lucas/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package silva.lucas.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import silva.lucas.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
---
+++ src/main/java/silva/lucas/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package silva.lucas.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import silva.lucas.cursomc.domain.Categoria;
+import silva.lucas.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		}
+	
+}
