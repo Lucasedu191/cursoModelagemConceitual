fbbaeb5dfc08e93b6a277eb27e988774f00a0725
Lucas Eduardo <lucasatletico@hotmail.com>
2020-11-03 17:08:05 -0300

    Produtos e associacao muitos para muitos com categaria

--- src/main/java/silva/lucas/cursomc/domain/Categoria.java
+++ src/main/java/silva/lucas/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package silva.lucas.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
@@ -16,6 +19,12 @@ public class Categoria implements Serializable{
 		@GeneratedValue(strategy=GenerationType.IDENTITY)
 		private Integer id;
 		private String nome;
+		
+		@ManyToMany(mappedBy="categorias")
+		private List<Produto> produtos = new ArrayList<>();
+		
+		
+		
 		
 		public Categoria() {
 			
@@ -43,6 +52,14 @@ public class Categoria implements Serializable{
 			this.nome = nome;
 		}
 
+		public List<Produto> getProdutos() {
+			return produtos;
+		}
+
+		public void setProdutos(List<Produto> produtos) {
+			this.produtos = produtos;
+		}
+
 		@Override
 		public int hashCode() {
 			final int prime = 31;
@@ -67,5 +84,6 @@ public class Categoria implements Serializable{
 				return false;
 			return true;
 		}
+
 		
 	}
---
+++ src/main/java/silva/lucas/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package silva.lucas.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import silva.lucas.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
