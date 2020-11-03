ba7348467dffe812d78e071a83b0cbbe1b07ba79
Lucas Eduardo <lucasatletico@hotmail.com>
2020-11-03 18:57:58 -0300

    Ajustes no endpoint / categorias / {id}

--- src/main/java/silva/lucas/cursomc/services/CategoriaService.java
+++ src/main/java/silva/lucas/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package silva.lucas.cursomc.services;
 
 import java.util.Optional;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +14,11 @@ public class CategoriaService {
 	
 	@Autowired
 	private CategoriaRepository repo;
+	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName(), null));
 		}
 	
 }
