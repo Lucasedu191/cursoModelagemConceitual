70ddddfd3c61a39b11ec7cbb2d28c479994c2a56
Lucas Eduardo <lucasatletico@hotmail.com>
2020-11-23 08:57:56 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/silva/lucas/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package silva.lucas.cursomc.services;
+
+import java.util.Optional;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import silva.lucas.cursomc.domain.Pedido;
+import silva.lucas.cursomc.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName(), null));
+		}
+	
+}
