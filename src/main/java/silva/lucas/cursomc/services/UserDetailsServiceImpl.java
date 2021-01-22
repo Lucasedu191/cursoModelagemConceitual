fb52c7f91bc93597877529ef561baef6fe3f2ee5
Lucas Eduardo <lucasatletico@hotmail.com>
2021-01-22 20:54:33 -0300

    implementando autenticacao e geracao de token JWT

---
+++ src/main/java/silva/lucas/cursomc/services/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package silva.lucas.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import silva.lucas.cursomc.domain.Cliente;
+import silva.lucas.cursomc.repositories.ClienteRepository;
+import silva.lucas.cursomc.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
