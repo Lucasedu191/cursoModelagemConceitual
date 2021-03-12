791f47d36627c9c0079da141407e63d497e6e678
Lucas Eduardo <lucasatletico@hotmail.com>
2021-03-12 16:31:32 -0300

    Enviando imagem via endpoint

--- src/main/java/silva/lucas/cursomc/services/ClienteService.java
+++ src/main/java/silva/lucas/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package silva.lucas.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import silva.lucas.cursomc.domain.Cidade;
 import silva.lucas.cursomc.domain.Cliente;
@@ -40,6 +42,9 @@ public class ClienteService {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private S3Service s3service;
 	
 	public Cliente find(Integer id) {
 		
@@ -108,5 +113,8 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3service.uploadFile(multipartFile);
 	}
 }
