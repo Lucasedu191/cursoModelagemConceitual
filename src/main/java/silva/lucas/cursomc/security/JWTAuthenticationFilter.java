aabaf3de7a5a6c2c59f5629678bca3ec4810a595
Lucas Eduardo <lucasatletico@hotmail.com>
2021-03-19 18:39:58 -0300

    expondo o header authorization

--- src/main/java/silva/lucas/cursomc/security/JWTAuthenticationFilter.java
+++ src/main/java/silva/lucas/cursomc/security/JWTAuthenticationFilter.java
@@ -2,7 +2,7 @@ package silva.lucas.cursomc.security;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
+//import java.util.Date;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -13,7 +13,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+//import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -26,7 +26,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	private JWTUtil jwtUtil;
 	
 	public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
-		setAuthenticationFailureHandler(new JWTAuthenticationFailureHandler());
+		//setAuthenticationFailureHandler(new JWTAuthenticationFailureHandler());
 		this.authenticationManager = authenticationManager;
 		this.jwtUtil = jwtUtil;
 	}
@@ -55,10 +55,11 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		String username = ((UserSS) auth.getPrincipal()).getUsername();
 		String token = jwtUtil.generateToken(username);
 		res.addHeader("Authorization", "Bearer" + token);
+		res.addHeader("access-control-expose-headers", "Authorization");
 		 
 	}
 	
-	private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
+	 /* private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
 		 
         @Override
         public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
@@ -78,6 +79,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         }
 
 	}
+	*/
 }
 
 
