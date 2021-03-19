97e6cc73f112c09d0406ffd50245145a0fdec9e3
Lucas Eduardo <lucasatletico@hotmail.com>
2021-03-19 18:21:55 -0300

    ajustando o tamanho da imagem

--- src/main/java/silva/lucas/cursomc/services/ImageService.java
+++ src/main/java/silva/lucas/cursomc/services/ImageService.java
@@ -10,6 +10,7 @@ import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FilenameUtils;
 import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,13 +22,13 @@ public class ImageService {
 
 	public BufferedImage getJpgImageFromFile(MultipartFile uploadedFile) {
 		String ext = FilenameUtils.getExtension(uploadedFile.getOriginalFilename());
-		if(!"png".equals(ext) && !"jpg" .equals(ext)) {
+		if (!"png".equals(ext) && !"jpg".equals(ext)) {
 			throw new FileException("Somente imagens PNG e JPG são permitidas");
 		}
-		
+
 		try {
 			BufferedImage img = ImageIO.read(uploadedFile.getInputStream());
-			if("png".equals(ext)) {
+			if ("png".equals(ext)) {
 				img = pngTojpg(img);
 			}
 			return img;
@@ -37,22 +38,36 @@ public class ImageService {
 	}
 
 	public BufferedImage pngTojpg(BufferedImage img) {
-		BufferedImage jpgImage = new BufferedImage(img.getWidth(), img.getHeight(),
-				BufferedImage.TYPE_INT_RGB);
+		BufferedImage jpgImage = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
 		jpgImage.createGraphics().drawImage(img, 0, 0, Color.WHITE, null);
 		return jpgImage;
 	}
-	
+
 	public InputStream getInputStream(BufferedImage img, String extension) {
 		try {
 			ByteArrayOutputStream os = new ByteArrayOutputStream();
 			ImageIO.write(img, extension, os);
 			return new ByteArrayInputStream(os.toByteArray());
-		}catch(IOException e) {
+		} catch (IOException e) {
 			throw new FileException("Erro ao ler arquivo");
 		}
 	}
+
+	public BufferedImage cropSquare(BufferedImage sourceImg) {
+		int min = (sourceImg.getHeight() <= sourceImg.getWidth()) ? sourceImg.getHeight() : sourceImg.getWidth();
+		return Scalr.crop(
+				sourceImg, 
+				(sourceImg.getWidth() / 2) - (min / 2), 
+				(sourceImg.getHeight() / 2) - (min / 2),
+				min,
+				min);
+	}
+	
+	public BufferedImage resize(BufferedImage sourceImg, int size) {
+		return Scalr.resize(sourceImg, Scalr.Method.QUALITY, size);
+	}
 }
+
 
 
 
